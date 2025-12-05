609b012f4bc7be8bb74ed609d1045e60b7004b67
gydnjsdl <khw33352182@gmail.com>
2025-12-05 10:56:07 +0900

    project start: add project file

---
+++ src/main/java/com/example/frac/FracApplication.java
@@ -0,0 +1,13 @@
+package com.example.frac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FracApplication.class, args);
+	}
+
+}
