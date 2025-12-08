1fee907757d2122fc31511f733747a0bfd6ffcd4
gydnjsdl <khw33352182@gmail.com>
2025-12-08 13:34:25 +0900

    feat: add signup page and mapping

--- src/main/java/com/example/frac/Controllers/AccountController.java
+++ src/main/java/com/example/frac/Controllers/AccountController.java
@@ -1,4 +1,41 @@
 package com.example.frac.Controllers;
 
-public class AccountController {
+import com.example.frac.Entity.Account;
+import com.example.frac.Servicece.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AccountController
+{
+    private final AccountService accountService;
+
+    @PostMapping("account/signup")
+    public String SignUp(
+            @RequestParam String username,
+            @RequestParam String password
+    )
+    {
+        return "";
+    }
+    @GetMapping("/account/signup")
+    public String SignUp()
+    {
+        return "signup";
+    }
+
+
+
+    @GetMapping("/account/signin")
+    public String SignIn()
+    {
+        return "signup";
+    }
+
+    public AccountController(AccountService _accountService)
+    {
+        accountService = _accountService;
+    }
 }
