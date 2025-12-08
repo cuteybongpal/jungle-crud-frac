4b3846bf38443c3c1097a6b2ae9698d96f505519
gydnjsdl <khw33352182@gmail.com>
2025-12-08 13:38:31 +0900

    feat: add article list to main page

--- src/main/java/com/example/frac/Controllers/MainController.java
+++ src/main/java/com/example/frac/Controllers/MainController.java
@@ -1,14 +1,27 @@
 package com.example.frac.Controllers;
 
+import com.example.frac.Entity.Article;
+import com.example.frac.Servicece.ArticleService;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 public class MainController
 {
+    private final ArticleService articleService;
     @GetMapping("/")
-    public String Index()
+    public String Index(Model model)
     {
+        List<Article> articles = articleService.GetAllArticle();
+        model.addAttribute("articles", articles);
         return "index.html";
+    }
+
+    public MainController(ArticleService _articleService)
+    {
+        articleService = _articleService;
     }
 }
